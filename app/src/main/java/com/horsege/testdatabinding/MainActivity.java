d1fab737220d5f051a2f53b72400930eb92b54ba
mamingzhang <mamingzhang@shuzijiayuan.com>
2018-01-19 16:01:42 +0800

    Observable Binding

--- app/src/main/java/com/horsege/testdatabinding/MainActivity.java
+++ app/src/main/java/com/horsege/testdatabinding/MainActivity.java
@@ -1,13 +1,17 @@
 package com.horsege.testdatabinding;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.horsege.testdatabinding.databinding.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler handler = new Handler();
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +19,23 @@ public class MainActivity extends AppCompatActivity {
 
         MainActivityBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        User user = new User("First Name", "Last Name");
+        user = new User("First Name", "Last Name");
         binding.setUser(user);
 
 //        Intent intent = new Intent(this, SecondActivity.class);
 //        startActivity(intent);
+
+        testObservableObjects();
+    }
+
+    private void testObservableObjects() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                user.setFirstName("FirstName Changed");
+                user.setLastName("LastName Changed");
+                user.age.set(30);
+            }
+        }, 5000);
     }
 }
--- app/src/main/java/com/horsege/testdatabinding/User.java
+++ app/src/main/java/com/horsege/testdatabinding/User.java
@@ -1,23 +1,42 @@
 package com.horsege.testdatabinding;
 
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+import android.databinding.ObservableInt;
+
 /**
  * Created by mamingzhang on 2018/1/19.
  */
 
-public class User {
-    private final String firstName;
-    private final String lastName;
+public class User extends BaseObservable {
+    private String firstName;
+    private String lastName;
+
+    public ObservableInt age = new ObservableInt(0);
 
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    @Bindable
     public String getFirstName() {
         return this.firstName;
     }
 
+    @Bindable
     public String getLastName() {
         return this.lastName;
     }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+        notifyPropertyChanged(BR.firstName);
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+        notifyPropertyChanged(BR.lastName);
+    }
+
 }
